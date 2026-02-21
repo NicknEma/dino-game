8407d2ac8246002fefc8011afacff6b4e6ebf934
NicknEma <62065135+NicknEma@users.noreply.github.com>
2026-02-21 19:14:25 +0100

    Set executable icon

--- build.bat
+++ build.bat
@@ -5,5 +5,8 @@ set opts=-debug
 
 if not exist build mkdir build
 pushd build
-odin build %root%\source %opts% -out:dino.exe
+del *.pdb > NUL 2> NUL
+rc /nologo /fodino_resources.res %root%\source\dino_resources.rc
+odin build %root%\source %opts% -out:dino.exe -extra-linker-flags:"dino_resources.res"
+del *.res > NUL 2> NUL
 popd
